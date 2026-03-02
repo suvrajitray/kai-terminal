95f9cba32079f34c99f7b2722b696edb337328c8
Suvrajit Ray <suvrajit.ray@gmail.com>
2026-03-02 20:59:58 +0530

    deleted unused files.

--- backend/KAITerminal.Broker.Obsolete/Models/UpstoxSessionResponse.cs
+++
@@ -1,4 +0,0 @@
-namespace KAITerminal.Broker.Models;
-
-public record UpstoxSessionResponse(string Status, UpstoxSessionData Data);
-public record UpstoxSessionData(string AccessToken, string UserId, string Email, string UserName);
